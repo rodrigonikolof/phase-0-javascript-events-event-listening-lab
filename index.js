c87998f04b55ee9efa12773dc025868ad7fcf99b
Rodrigo Nikolof <rodrigo.nikolof@outlook.com>
2022-10-02 15:08:06 +1100

    commit tree bdb42fa203cfe263736940c4293968c39202d140

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
 
 
 
-function addingEventListener() {
+// function addingEventListener() {
 
-    const input = document.getElementById('button');
-    input.addEventListener('click', function() {
-      alert('I was clicked!');
-    })
-}
+//     const input = document.getElementById('button');
+//     input.addEventListener('click', function() {
+//       alert('I was clicked!');
+//     })
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
